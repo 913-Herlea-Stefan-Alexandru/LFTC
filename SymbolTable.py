51b8ead4557dbcf5d00e8e1b71c8be7f3b8d3a2e
913 Stefan Alexandru Herlea <herleastefan@yahoo.com>
2022-10-26 14:12:45 +0300

    added symbol table class

---
+++ SymbolTable.py
@@ -0,0 +1,16 @@
+from HashTable import HashTable
+
+
+class SymbolTable:
+
+    def __init__(self):
+        self.index = 0
+        self.identifiersTable = HashTable()
+        self.constantsTable = HashTable()
+
+    def add_identifier(self, identifier):
+        self.identifiersTable.insert(identifier, self.index)
+        self.index += 1
+
+    def add_constant(self, constant):
+        self.constantsTable.insert(constant, constant)
